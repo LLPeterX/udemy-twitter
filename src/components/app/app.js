13b9e253144139af93227b6de82d523f4c77d255
LLPeterX <LPeter@mail.ru>
2020-07-27 13:19:16 +0300

    sample class

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,7 +4,7 @@ import AppHeader from '../app-header';
 import SearchPanel from '../search-panel/search-panel'
 import PostStatusFilter from '../post-status-filter'
 import PostList from '../post-list'
-import PostAddForm from '../post-add-form/post-add-form';
+import PostAddForm from '../post-add-form'
 
 const App = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -3,9 +3,47 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/';
 
+//let counter = 0;
+
+// function WhoIAm(props) {
+//   counter++;
+//   return (
+//     <>
+//       <h1>{counter}) My name is {props.name}, surname - {props.surname}</h1>
+//       <a href={props.link}>My Profile</a>
+//     </>
+//   );
+// }
+
+class WhoIAm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.counter = 1; // собственное состояние
+  }
+  render() {
+    this.counter++;
+    return (
+      <>
+        <h1>{this.counter}) My name is {this.props.name}, surname - {this.props.surname}</h1>
+        <a href={this.props.link}>My Profile</a>
+      </>
+    );
+  }
+}
+
+function All() {
+  return (
+    <>
+      <WhoIAm name="Вася" surname="Иванов" link="https://www.google.ru" />
+      <WhoIAm name="Петр" surname="Лебедев" link="https://vk.com" />
+    </>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <All />
   </React.StrictMode>,
   document.getElementById('root')
 );
