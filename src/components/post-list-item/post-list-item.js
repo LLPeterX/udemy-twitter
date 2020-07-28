31a13cc076e3b9340d7ea12ba52e4185e85986f0
LLPeterX <LPeter@mail.ru>
2020-07-28 17:25:15 +0300

    refactor PostListItem as class component

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,31 +1,33 @@
 import React from 'react';
 import './post-list-item.css';
 
-const PostListItem = ({ label, important=false }) => {
-  let classNames = "app-list-item d-flex justify-content-between";
-  if(important) {
-    classNames+=" important";
+export default class PostListItem extends React.Component {
+  render() {
+    const {label, important = false } = this.props;
+    let classNames = "app-list-item d-flex justify-content-between";
+    if (important) {
+      classNames += " important";
+    }
+    return (
+      <li className={classNames}>
+        <span className="app-list-item-label">
+          {label}
+        </span>
+        <div className="d-flex ustify-content-center align-items-center">
+          <button
+            type="button"
+            className="btn-star btn-sm">
+            <i className="fa fa-star"></i>
+          </button>
+          <button
+            type="button"
+            className="btn-trash btn-sm">
+            <i className="fa fa-trash-o"></i>
+          </button>
+          <i className="fa fa-heart-o"></i>
+        </div>
+      </li>
+    );
   }
-  return (
-    <li className={classNames}>
-      <span className="app-list-item-label">
-        {label}
-      </span>
-      <div className="d-flex ustify-content-center align-items-center">
-        <button
-          type="button"
-          className="btn-star btn-sm">
-          <i className="fa fa-star"></i>
-        </button>
-        <button
-          type="button"
-          className="btn-trash btn-sm">
-          <i className="fa fa-trash-o"></i>
-        </button>
-        <i className="fa fa-heart-o"></i>
-      </div>
-    </li>
-  );
 }
 
-export default PostListItem;
